b73757a7cce2be9bf74a6450eac59a76654100fb
JXYU2004 <yujixiangftw@gmail.com>
2026-05-14 23:09:33 +0800

    Implement testing

---
+++ test.java
@@ -0,0 +1,13 @@
+class Ship {
+    private String name;
+    private int yearBuilt;
+
+    public Ship(String name, int yearBuilt) {
+        this.name = name;
+        
+    }
+
+    public String getName() {
+        return name;
+    }
+}
