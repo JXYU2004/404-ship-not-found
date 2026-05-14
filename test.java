246e0b07bde4c8e40765cc79ecf699257dfbd4a2
idkwattopt-689 <ziyanghe8@gmail.com>
2026-05-14 23:17:37 +0800

    Implemented testing 2

--- test.java
+++ test.java
@@ -1,10 +1,11 @@
 class Ship {
     private String name;
     private int yearBuilt;
+    private int year = 2100;
 
     public Ship(String name, int yearBuilt) {
         this.name = name;
-        
+        this.yearBuilt = yearBuilt;
     }
 
     public String getName() {
